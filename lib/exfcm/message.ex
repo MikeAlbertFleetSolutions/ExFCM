78e92473031a880aa9abde349b3b456b4fdd85b0
waynesibley <Wayne.Sibley@mikealbert.com>
2018-04-12 18:12:05 -0400

    Done playing for now

--- lib/exfcm/message.ex
+++ lib/exfcm/message.ex
@@ -2,56 +2,34 @@ defmodule ExFCM.Message do
 
   @moduledoc """
   This module is used to manage and send Messages.
-
   To send a notification you have to provide a target using `target_device` (for a single device or group of devices) or using a `target_topic`.
-
   ## Examples
-
   ```
-
   {:ok , result } = Message.put_data(%{"sample" => "true"})
   |> Message.put_notification("Title","Description")
   |> Message.target_topic("sample_giveaways")
   |> Message.send
-
   ```
-
   """
 
   require Logger
 
-  defstruct to: "", notification: nil, data: nil, new_notification: nil
+  defstruct to: "", notification: nil, data: nil
 
   @url Application.get_env(:exfcm, :fcm_url, "")
   @server_key Application.get_env(:exfcm, :server_key, "")
 
-#  defmodule Notification do
-#    defstruct title: nil, body: nil
-#  end
-
-  @doc """
-  Puts a Notification inside message. It will be displayed in tray when app is in background.
-  """
-
-#  def put_notification(message \\ %__MODULE__{}, title, data) do
-#    notification = %Notification{ title: title, body: data}
-#    %__MODULE__{message | notification: notification}
-#  end
-
-  @doc """
-  Puts a Notification inside message. It will be displayed in tray when app is in background.
-  """
-
-  def put_notification(message \\ %__MODULE__{}, title, body) do
-    %__MODULE__{message | notification: %{"title" => title, "body" => body}}
+  defmodule Notification do
+    defstruct title: nil, body: nil
   end
 
   @doc """
   Puts a Notification inside message. It will be displayed in tray when app is in background.
   """
 
-  def put_notification(message \\ %__MODULE__{}, new_notification) do
-    %__MODULE__{message | new_notification: new_notification}
+  def put_notification(message \\ %__MODULE__{}, title, data) do
+    notification = %Notification{ title: title, body: data}
+    %__MODULE__{message | notification: notification}
   end
 
   @doc """
@@ -72,7 +50,6 @@ defmodule ExFCM.Message do
 
   @doc """
   Sets target of notification. It should be only the name of the topic without "/topics/name".
-
   Topic has to match ``` [a-zA-Z0-9-_.~%]+  ``` regex.
   """
 
